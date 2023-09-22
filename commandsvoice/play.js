cfbc7549b39681562a0e891bda42a9e48d2e8a77
Paul Bagot <bagotpa@gmail.com>
2023-09-22 15:28:42 +0200

    fix: the bot no longer crashes when it can not find songs

--- commandsvoice/play.js
+++ commandsvoice/play.js
@@ -2,18 +2,28 @@ exports.run = (client, message, args) => {
 
     let Args = args.join(' ');
     let voiceChannel = message.member.voice.channel;
-    if(!voiceChannel)
+    if(!voiceChannel) {
         message.reply("vous n'êtes pas connecté");
-    else if(!Args)
-        message.reply("vous n'avez rien demandé à jouer");
-    else {
-        client.distube.play(voiceChannel, Args, {
-            member: message.member,
-            position: 0,
-            textChannel: message.channel,
-        });
-        message.delete();
+        return;
     }
+    if(!Args) {
+        message.reply("vous n'avez rien demandé à jouer");
+        return;
+    }
+
+    async function searchSong() {
+        await client.distube.search(Args, {safeSearch : true})
+        .then((tab) => {
+            client.distube.play(voiceChannel, tab[0], {
+                member: message.member,
+                position: 0,
+                textChannel: message.channel,
+            });
+            message.delete();
+        })
+        .catch((err) => message.channel.send("aucun son trouvé"))
+    }
+    searchSong()
 }
 
 exports.name = "play";

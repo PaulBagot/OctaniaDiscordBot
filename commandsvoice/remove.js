481edcb6eab2d2aad06cbe8cfcf24e7ea2de20c2
Paul Bagot <bagotpa@gmail.com>
2023-09-26 22:35:13 +0200

    fix: fix display the wrong song to remove

--- commandsvoice/remove.js
+++ commandsvoice/remove.js
@@ -10,9 +10,9 @@ exports.run = (client, message, args) => {
     if(queue.songs[index] == null) return
     if(index <= 0) return
 
-    queue.songs.splice(index, 1)
     message.channel.send(`:white_check_mark:  | \`${queue.songs[index].name}\` a été enlevé`)
     message.delete()
+    queue.songs.splice(index, 1)
 }
 
 exports.name = 'remove';

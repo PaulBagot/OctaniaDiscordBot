655c755474052152a4c11279d05cd71a4b4047ea
Paul Bagot <bagotpa@gmail.com>
2023-10-06 20:44:41 +0200

    fix : fix play command?

--- commandsvoice/play.js
+++ commandsvoice/play.js
@@ -12,16 +12,27 @@ exports.run = (client, message, args) => {
     }
 
     async function searchSong() {
-        await client.distube.search(Args, {safeSearch : true})
-        .then((tab) => {
-            client.distube.play(voiceChannel, tab[0], {
+        try {
+            let song = await client.distube.search(Args, {safeSearch : true});
+
+            client.distube.play(song, tab[0], {
                 member: message.member,
                 position: 0,
                 textChannel: message.channel,
             });
             message.delete();
-        })
-        .catch((err) => message.channel.send("aucun son trouvé"))
+        } catch(err) {
+            try {
+                client.distube.play(Args, tab[0], {
+                    member: message.member,
+                    position: 0,
+                    textChannel: message.channel,
+                });
+                message.delete();
+            } catch(err) {
+                message.channel.send("aucun son trouvé");
+            }
+        }
     }
     searchSong()
 }
